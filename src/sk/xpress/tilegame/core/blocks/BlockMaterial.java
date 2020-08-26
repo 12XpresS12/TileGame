19b7cda9e1e6c93cc86421ef42e0ae80bb79d582
12XpresS12 <12XpresS12@gmail.com>
2020-08-26 17:27:01 +0200

    BlockMaterial

--- src/sk/xpress/tilegame/core/blocks/BlockMaterial.java
+++ src/sk/xpress/tilegame/core/blocks/BlockMaterial.java
@@ -2,8 +2,12 @@ package sk.xpress.tilegame.core.blocks;
 
 import sk.xpress.tilegame.core.threads.Game;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public final class BlockMaterial {
 
@@ -14,24 +18,25 @@ public final class BlockMaterial {
 
     private int id;
     private String name;
-    private Color color;
 
     private BufferedImage bufferedImage;
 
-    public BlockMaterial(int id, String name, Color color) {
+    public BlockMaterial(int id, String name, String bufferedImagePath) {
         this.id = id;
-        this.color = color;
         this.name = name;
 
-        bufferedImage = new BufferedImage(Game.DEFAULT_TILE_SIZE_PX, Game.DEFAULT_TILE_SIZE_PX, BufferedImage.TYPE_INT_ARGB);
+        System.out.println("X:" +  BlockMaterial.class.getClassLoader().getResource("water.png"));
+        try {
+            bufferedImage = ImageIO.read(BlockMaterial.class.getClassLoader().getResource("" + bufferedImagePath + ".png"));
 
-        if(bufferedImage == null)
-            return;
-
-        for(int x = 0; x < Game.DEFAULT_TILE_SIZE_PX; x++) {
-            for(int y = 0; y < Game.DEFAULT_TILE_SIZE_PX; y++) {
-                bufferedImage.setRGB(x, y, color.getRGB());
+        } catch (IOException e) {
+            bufferedImage = new BufferedImage(Game.DEFAULT_TILE_SIZE_PX, Game.DEFAULT_TILE_SIZE_PX, BufferedImage.TYPE_INT_ARGB);
+            for(int x = 0; x < Game.DEFAULT_TILE_SIZE_PX; x++) {
+                for(int y = 0; y < Game.DEFAULT_TILE_SIZE_PX; y++) {
+                    bufferedImage.setRGB(x, y, Color.PINK.getRGB());
+                }
             }
+            e.printStackTrace();
         }
     }
 
@@ -45,15 +50,13 @@ public final class BlockMaterial {
         return name;
     }
 
-    public Color getColor() {
-        return color;
-    }
-
     static {
         System.out.println("Initializing block materials");
-        WATER = (new BlockMaterial(0, "water", new Color(35, 137, 218)));
-        STONE = (new BlockMaterial(1, "stone", new Color(136,140,141)));
-        GRASS_BLOCK = (new BlockMaterial(2, "grass_block", new Color(11,102,35)));
-        WOOD_PLANK = (new BlockMaterial(2, "wood_plank", new Color(100,100,100)));
+        WATER = (new BlockMaterial(0, "water", "water"));
+        STONE = (new BlockMaterial(1, "stone", "stone"));
+        GRASS_BLOCK = (new BlockMaterial(2, "grass_block", "grass_block"));
+        WOOD_PLANK = (new BlockMaterial(3, "wood_plank", "wood_plank"));
     }
+
+
 }
